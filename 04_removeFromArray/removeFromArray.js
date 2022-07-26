15479176dbf0cbe36d335736b5698c794586dc14
Justin Howell <justinmhowell@protonmail.com>
2022-07-26 15:57:48 -0400

    Solve the exercise

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,6 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ...args) {
+    let newArray = array.filter(i => !args.includes(i));
+    return newArray;
 };
 
 // Do not edit below this line
