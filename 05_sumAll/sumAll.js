9d8ff6ecc1f0bd702a466555aaea2d0ce4a5075f
Justin Howell <justinmhowell@protonmail.com>
2022-07-26 21:38:16 -0400

    Solve the exercise

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,23 @@
-const sumAll = function() {
-
+const sumAll = function(a, b) {
+    let sum;
+    if ((typeof a) !== "number" || (typeof b) !== "number") {
+        return "ERROR";
+    }
+    if (a < 0 || b < 0) {
+        return "ERROR";
+    }
+    if (a < b) {
+        sum = b;
+        for (i = a; i < b; i++) {
+            sum += i;
+        }
+    } else {
+        sum = a;
+        for (i = b; i < a; i++) {
+            sum += i;
+        }
+    }
+    return sum;
 };
 
 // Do not edit below this line
