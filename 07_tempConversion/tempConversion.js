88fed8443a07b4e9f6088d68aa51076032f4b981
Justin Howell <justinmhowell@protonmail.com>
2022-07-26 22:06:14 -0400

    Solve the exercise

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,11 @@
-const ftoc = function() {
-
+const ftoc = function(tempF) {
+  let newTemp = (tempF - 32) * .5556;
+  return parseFloat(newTemp.toFixed(1));
 };
 
-const ctof = function() {
-
+const ctof = function(tempC) {
+  let newTemp = (tempC * 1.8) + 32;
+  return parseFloat(newTemp.toFixed(1));
 };
 
 // Do not edit below this line
