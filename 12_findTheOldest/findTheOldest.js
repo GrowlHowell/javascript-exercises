5323156d8e8031e534e5b99a3b171db98d16e84d
Justin Howell <justinmhowell@protonmail.com>
2022-08-02 13:56:37 -0400

    Solve the exercise

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,4 +1,14 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    const date = new Date();
+    const currentYear = date.getFullYear();
+    const oldestPerson = people.reduce((winner, person) => {
+        if (((person.yearOfDeath || currentYear) - person.yearOfBirth) > ((winner.yearOfDeath || currentYear) - winner.yearOfBirth)) {
+            return person;
+        } else if (((person.yearOfDeath || currentYear) - person.yearOfBirth) <= ((winner.yearOfDeath || currentYear) - winner.yearOfBirth)) {
+            return winner;
+        }
+    });
+    return oldestPerson;
 
 };
 
