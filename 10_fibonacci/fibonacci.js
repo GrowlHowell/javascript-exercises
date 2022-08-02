947c67280c7c168de60ccc88681371b258261f73
Justin Howell <justinmhowell@protonmail.com>
2022-08-02 11:09:26 -0400

    Solve the exercise

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,16 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    let array = [0, 1];
+    let num1 = 0;
+    let num2 = 1;
+    if (num < 0 || typeof num === String) {
+        return "OOPS";
+    }
+    for (let i = 0; i <= num; i++) {
+        array.push(array[num1] + array[num2]);
+        num1 += 1;
+        num2 += 1;
+    }
+    return array[num];
 };
 
 // Do not edit below this line
