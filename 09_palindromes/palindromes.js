ef2336a315b9310324633d81e7f05b16881cc2ae
Justin Howell <justinmhowell@protonmail.com>
2022-08-01 23:13:34 -0400

    Solve the exercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,8 @@
-const palindromes = function () {
-
+const palindromes = function(a) {
+    const garbage = /[\W_]/g;
+    const aLower = a.toLowerCase().replace(garbage, '');
+    const aReverse = aLower.split('').reverse().join('');
+    return aLower === aReverse;
 };
 
 // Do not edit below this line
